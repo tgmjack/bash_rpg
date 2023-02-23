7329628b2bf029c36f34191033f41907768178f4
tgmjack <52379214+tgmjack@users.noreply.github.com>
2023-02-23 14:06:14 +0000

    Update mi_game.sh

--- mi_game.sh
+++ mi_game.sh
@@ -1,11 +1,11 @@
 #to do list
-#1 left right and down
+#1 moving right seems to break things
 #2 detect if moving onto grass or not, can only move onto grass
 #3 enemies
 
 
 not_end_game=$true;
-your_xpos=8;
+your_xpos=6;
 your_ypos=5;
 cursor_xpos=0;
 cursor_ypos=0;
@@ -30,15 +30,15 @@ while [ $cursor_ypos -lt $screen_height ]
                         then
                                 this_line=$this_line$you
                         else
-                                R=$(($RANDOM%6))
-                                if [ $R == 1 ]|[ $R == 2 ]|[ $R == 3 ]
+                                R=$(($RANDOM%5))
+                                if [ $R == 0 ] || [ $R == 1 ] || [ $R == 2 ] || [ $R == 3 ]
                                         then
                                                 this_line=$this_line$grass
                                         else
                                                 this_line=$this_line$tree
                                 fi
 
-                fi
+                        fi
                 cursor_xpos=$((cursor_xpos+1))
                 done
                 cursor_ypos=$((cursor_ypos+1))
@@ -74,13 +74,13 @@ while [ not_end_game ]
                         fi
 
                 done
+        echo "=============="
+        echo "y = you       # = grass        T = tree      G = golbin"
+        old_pos=$((($screen_width + 1) * $your_ypos + $your_xpos))
 
-
-        old_pos=$(((($screen_width+1)*$your_ypos)+$your_xpos)
         read player_move
         if [ $player_move == "w" ]
                 then
-
                         new_pos=$(((($screen_width+1)*($your_ypos-1))+$your_xpos))
                         your_ypos=$((your_ypos-1))
 
@@ -100,8 +100,9 @@ while [ not_end_game ]
 
         elif [ $player_move == "a" ]
                 then
+
                         new_pos=$(((($screen_width+1)*($your_ypos))+$your_xpos-1))
-                        your_ypos=$((your_ypos-1))
+                        your_xpos=$((your_xpos-1))
                         # update for new_pos
                         new_char="y"
                         prefix=${whole_map:0:$((new_pos-1))}
@@ -114,19 +115,46 @@ while [ not_end_game ]
                         suffix=${whole_map:$old_pos}
                         whole_map="$prefix$new_char$suffix"
 
-                        u
+
         elif [ $player_move == "s" ]
                 then
-                        your_ypos=$your_ypos+1
-                        whole_map[$your_xpos,$your_ypos-1]=$grass
-                        whole_map[$your_xpos,$your_ypos]=$you
+                        new_pos=$(((($screen_width+1)*($your_ypos+1))+$your_xpos))
+                        your_ypos=$((your_ypos+1))
+                        # update for new_pos
+                        new_char="y"
+                        prefix=${whole_map:0:$((new_pos-1))}
+                        suffix=${whole_map:$new_pos}
+                        whole_map="$prefix$new_char$suffix"
+
+                        # update for old pos
+                        new_char="#"
+                        prefix=${whole_map:0:$((old_pos-1))}
+                        suffix=${whole_map:$old_pos}
+                        whole_map="$prefix$new_char$suffix"
+
+
         elif [ $player_move == "d" ]
                 then
-                        your_xpos=$your_xpos+1
-                        whole_map[$your_xpos-1,$your_ypos]=$grass
-                        whole_map[$your_xpos,$your_ypos]=$you
-                fi
+                        new_pos=$(((($screen_width+1)*($your_ypos))+$your_xpos+1))
+                        your_xpos=$((your_xpos+1))
+
+                        # update for new_pos
+                        new_char="y"
+                        prefix=${whole_map:0:$((new_pos-1))}
+                        suffix=${whole_map:$new_pos}
+                        whole_map="$prefix$new_char$suffix"
+
+                        # update for old pos
+                        new_char="#"
+                        prefix=${whole_map:0:$((old_pos-1))}
+                        suffix=${whole_map:$old_pos}
+                        whole_map="$prefix$new_char$suffix"
+
+        fi
+
+
+
+   done
 
 
 ######## do other moves below
-
